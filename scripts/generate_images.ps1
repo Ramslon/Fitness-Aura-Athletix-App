53934f8a3e095d6fe7a5215b0afbc4e2d4133ddb
Ramson Lonayo <ramsonlonayo@gmail.com>
2025-12-24 12:19:49 +0300

    Generate solid-color PNG thumbnails for exercise images

---
+++ scripts/generate_images.ps1
@@ -0,0 +1,77 @@
+Add-Type -AssemblyName System.Drawing
+
+$images = @(
+"assets/images/chest_flat_bench.png",
+"assets/images/chest_incline_bench.png",
+"assets/images/chest_decline_bench.png",
+"assets/images/chest_dumbbell_flies.png",
+"assets/images/chest_pullovers.png",
+"assets/images/chest_dips.png",
+"assets/images/chest_flat_dumbbell_press.png",
+"assets/images/chest_incline_dumbbell_press.png",
+"assets/images/chest_decline_dumbbell_press.png",
+
+"assets/images/back_bend_over_rows.png",
+"assets/images/back_dumbbell_rows.png",
+"assets/images/back_deadlift.png",
+"assets/images/back_pullups.png",
+"assets/images/back_tbar_row.png",
+"assets/images/back_lat_pull_down.png",
+"assets/images/back_seated_row.png",
+"assets/images/back_single_arm_rows.png",
+"assets/images/back_reverse_flys.png",
+"assets/images/back_bent_arm_pullovers.png",
+
+"assets/images/leg_back_squat.png",
+"assets/images/leg_goblet_squat.png",
+"assets/images/leg_front_squat.png",
+"assets/images/leg_sumo_squat.png",
+"assets/images/leg_romanian_deadlift.png",
+"assets/images/leg_conventional_deadlift.png",
+"assets/images/leg_walking_lunge.png",
+"assets/images/leg_reverse_lunge.png",
+"assets/images/leg_bulgarian_split.png",
+"assets/images/leg_extensions.png",
+"assets/images/leg_hamstring_curls.png",
+"assets/images/leg_glute_bridges.png"
+)
+
+$colors = @(
+[System.Drawing.Color]::FromArgb(255, 52, 152, 219), # blue
+[System.Drawing.Color]::FromArgb(255, 46, 204, 113), # green
+[System.Drawing.Color]::FromArgb(255, 231, 76, 60),  # red
+[System.Drawing.Color]::FromArgb(255, 155, 89, 182), # purple
+[System.Drawing.Color]::FromArgb(255, 241, 196, 15), # yellow
+[System.Drawing.Color]::FromArgb(255, 230, 126, 34), # orange
+[System.Drawing.Color]::FromArgb(255, 149, 165, 166),# gray
+[System.Drawing.Color]::FromArgb(255, 26, 188, 156), # teal
+[System.Drawing.Color]::FromArgb(255, 52, 73, 94)    # dark
+)
+
+foreach ($i in 0..($images.Count - 1)) {
+    $path = $images[$i]
+    $color = $colors[$i % $colors.Count]
+
+    $dir = [System.IO.Path]::GetDirectoryName($path)
+    if (-not (Test-Path $dir)) { New-Item -ItemType Directory -Path $dir | Out-Null }
+
+    $bmp = New-Object System.Drawing.Bitmap 600,400
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    $g.Clear($color)
+
+    # draw text label
+    $font = New-Object System.Drawing.Font('Segoe UI',24,[System.Drawing.FontStyle]::Bold)
+    $brush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::FromArgb(255,255,255))
+    $text = [System.IO.Path]::GetFileNameWithoutExtension($path) -replace '_',' '
+    $sf = New-Object System.Drawing.StringFormat
+    $sf.Alignment = [System.Drawing.StringAlignment]::Center
+    $sf.LineAlignment = [System.Drawing.StringAlignment]::Center
+    $rect = New-Object System.Drawing.RectangleF(0,0,600,400)
+    $g.DrawString($text, $font, $brush, $rect, $sf)
+
+    $bmp.Save($path, [System.Drawing.Imaging.ImageFormat]::Png)
+    $g.Dispose()
+    $bmp.Dispose()
+}
+
+Write-Output "Generated $($images.Count) images."
